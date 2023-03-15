2aea24b1b3b97a38a01f05484b66e80fa5b51ecd
Demian Füglistaler <demianfuglistaler@MacBook-Air-von-Demian.local>
2023-03-15 07:56:21 +0100

    catch try to  Handle OpenAI-API Error - openai.error.RateLimitError: Ratelimit Error

--- openAIClass.py
+++ openAIClass.py
@@ -18,21 +18,26 @@ class OpenAi:
 
 
     def checkAnswer(self, question, question_body):
-        blacklist = ["KI", "Als KI", "Als künstliche Intelligenz", "künstliche Intelligenz", "Künstliche Intelligenz", "Als AI", "AI", "künstlicher Intelligenzassistent", "Intelligenzassistent", "digitale Entitität"]
+        try:
+            blacklist = ["KI", "Als KI", "Als künstliche Intelligenz", "künstliche Intelligenz", "Künstliche Intelligenz", "Als AI", "AI", "künstlicher Intelligenzassistent", "Intelligenzassistent", "digitale Entitität"]
+            completion = openai.ChatCompletion.create(
+                model="gpt-3.5-turbo",
+                messages=[
+                    {
+                        "role": "assistant",
+                        "content": question + "-" + question_body
+                    }
+                ]
+            )
+            answer = completion.choices[0].message.content.strip('\n')
 
-        completion = openai.ChatCompletion.create(
-            model="gpt-3.5-turbo",
-            messages=[
-                {
-                    "role": "assistant",
-                    "content": question + "-" + question_body
-                }
-            ]
-        )
-        answer = completion.choices[0].message.content.strip('\n')
+            if any([x in answer for x in blacklist]):
+                return 'blacklist'
 
-        if any([x in answer for x in blacklist]):
-            return 'blacklist'
+            else:
+                return answer
+        except Exception as e:
+            func_name = sys._getframe().f_code.co_name
+            print(colored(func_name.upper() + " That AI model is probebly overloaded with other requests!!!", 'red'))
+            exit(colored(e, 'red'))
 
-        else:
-            return answer
